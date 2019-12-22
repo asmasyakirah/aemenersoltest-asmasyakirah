95987c4320adcdc67d4101b3d936a55adbeef671
asmasyakirah <asma.syakirahd@gmail.com>
2019-12-22 18:57:11 +0800

    completed all requirements

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -2,6 +2,7 @@ using System;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
+using System.Data.SqlTypes;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
@@ -33,10 +34,12 @@ public class Well
 public partial class _Default : System.Web.UI.Page
 {
     private static HttpClient httpClient = new HttpClient();
-    private const string URLAEMEnersol = "http://test-demo.aem-enersol.com/api/PlatformWell/GetPlatformWellDummy";
+    private const string URLAEMEnersol = "http://test-demo.aem-enersol.com/api/PlatformWell/GetPlatformWellActual";
+    private SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["LocalDBConn"].ConnectionString);
 
     string myUsername;
     string myToken;
+    Platform[] platform;
 
     protected void Page_Load(object sender, EventArgs e)
     {
@@ -58,12 +61,13 @@ public partial class _Default : System.Web.UI.Page
 
     protected void BtnSync_Click(object sender, EventArgs e)
     {
-        lblStatus.Text = "Sync button clicked";
+        //lblStatus.Text = "Sync button clicked";
 
-        SyncData();
+        // Get Platform and Well data from API
+        GetPlatformAndWell();
     }
 
-    private async void SyncData()
+    private async void GetPlatformAndWell()
     {
         var request = new HttpRequestMessage(HttpMethod.Get, URLAEMEnersol);
         request.Headers.Accept.Clear();
@@ -82,28 +86,137 @@ public partial class _Default : System.Web.UI.Page
             string responseBody = await response.Content.ReadAsStringAsync();
 
             // Deserialize responsebody JSON in string to Employee (array) object
-            Platform[] platform = Newtonsoft.Json.JsonConvert.DeserializeObject<Platform[]>(responseBody);
-            
-            foreach(Platform p in platform)
-            {
-                lblStatus.Text += "<br>" + p.UniqueName;
+            platform = Newtonsoft.Json.JsonConvert.DeserializeObject<Platform[]>(responseBody);
 
-                if(p.Well!=null&&p.Well.Length>0)
-                {
-                    foreach (Well w in p.Well)
-                    {
-                        lblStatus.Text += "<br>   " + w.UniqueName;
-                    }
-                }
-            }
+            //foreach (Platform p in platform)
+            //{
+            //    lblStatus.Text += "<br>" + p.UniqueName;
+
+            //    if (p.Well != null && p.Well.Length > 0)
+            //    {
+            //        foreach (Well w in p.Well)
+            //        {
+            //            lblStatus.Text += "<br>   " + w.UniqueName;
+            //        }
+            //    }
+            //}
 
             // Show success message
-            lblStatus.Text += "<br>SUCCESS";
+            //lblStatus.Text += "<br>SUCCESS";
+
+            SyncPlatformAndWell();
         }
         else
         {
             // Show failed message
-            lblStatus.Text = "FAILED<br>"+response.ToString();
+            lblStatus.Text = "Synchronization from API to localDB failed";
+            //lblStatus.Text = "FAILED<br>"+response.ToString();
         }
+    }
+
+    private void SyncPlatformAndWell()
+    {
+        DateTime now = DateTime.Now;
+
+        SyncPlatform();
+        SyncWell();
+
+        lblStatus.Text += "<br>Synchronization from API to localDB sucessfully done<br>on " + now.ToString("dd/MM/yyyy hh:mm tt");
+    }
+
+    private void SyncPlatform()
+    {
+        if (con.State == System.Data.ConnectionState.Closed)
+        { con.Open(); }
+
+        SqlCommand commandClear = new SqlCommand("DELETE FROM Platform", con);
+        commandClear.ExecuteNonQuery();
+
+        string commandInsert = "";
+        int platformCount = platform.Length;
+        foreach (Platform p in platform)
+        {
+            // Add to command insert
+            commandInsert += "(@Id" + p.Id + ", "
+                  + "@UniqueName" + p.Id + ", "
+                  + "@Latitude" + p.Id + ", "
+                  + "@Longitude" + p.Id + ", "
+                  + "@CreatedAt" + p.Id + ", "
+                  + "@UpdatedAt" + p.Id + ")";
+
+            platformCount--;
+            if(platformCount!=0)
+            {
+                commandInsert += ",";
+            }
+        }
+
+        SqlCommand command = new SqlCommand("INSERT INTO Platform ([Id],[UniqueName],[Latitude],[Longitude],[CreatedAt],[UpdatedAt]) VALUES "
+                  + commandInsert, con);
+
+        foreach (Platform p in platform)
+        {
+            command.Parameters.AddWithValue("@Id" + p.Id, p.Id);
+            command.Parameters.AddWithValue("@UniqueName" + p.Id, p.UniqueName != null ? p.UniqueName : string.Empty);
+            command.Parameters.AddWithValue("@Latitude" + p.Id, p.Latitude != null ? p.Latitude : string.Empty);
+            command.Parameters.AddWithValue("@Longitude" + p.Id, p.Longitude != null ? p.Longitude : string.Empty);
+            command.Parameters.AddWithValue("@CreatedAt" + p.Id, p.CreatedAt != null ? p.CreatedAt : string.Empty);
+            command.Parameters.AddWithValue("@UpdatedAt" + p.Id, p.UpdatedAt != null ? p.UpdatedAt : string.Empty);
+        }
+
+        command.ExecuteNonQuery();
+        con.Close();
+    }
+
+    private void SyncWell()
+    {
+        if (con.State == System.Data.ConnectionState.Closed)
+        { con.Open(); }
+
+        SqlCommand commandClear = new SqlCommand("DELETE FROM Well", con);
+        commandClear.ExecuteNonQuery();
+
+        foreach (Platform p in platform)
+        {
+            string commandInsert = "";
+            if (p.Well != null && p.Well.Length > 0)
+            {
+                int wellCount = p.Well.Length;
+                foreach (Well w in p.Well)
+                {
+                    // Add to command insert
+                    commandInsert += "(@Id" + w.Id + ", "
+                      + "@PlatformId" + w.Id + ", "
+                      + "@UniqueName" + w.Id + ", "
+                      + "@Latitude" + w.Id + ", "
+                      + "@Longitude" + w.Id + ", "
+                      + "@CreatedAt" + w.Id + ", "
+                      + "@UpdatedAt" + w.Id + ")";
+
+                    wellCount--;
+                    if (wellCount != 0)
+                    {
+                        commandInsert += ",";
+                    }
+                }
+
+                SqlCommand command = new SqlCommand("INSERT INTO Well (Id,PlatformId,UniqueName,Latitude,Longitude,CreatedAt,UpdatedAt) VALUES "
+                  + commandInsert, con);
+
+                foreach (Well w in p.Well)
+                {
+                    command.Parameters.AddWithValue("@Id" + w.Id, w.Id);
+                    command.Parameters.AddWithValue("@PlatformId" + w.Id, w.PlatformId);
+                    command.Parameters.AddWithValue("@UniqueName" + w.Id, w.UniqueName != null ? w.UniqueName : string.Empty);
+                    command.Parameters.AddWithValue("@Latitude" + w.Id, w.Latitude != null ? w.Latitude : string.Empty);
+                    command.Parameters.AddWithValue("@Longitude" + w.Id, w.Longitude != null ? w.Longitude : string.Empty);
+                    command.Parameters.AddWithValue("@CreatedAt" + w.Id, w.CreatedAt != null ? w.CreatedAt : string.Empty);
+                    command.Parameters.AddWithValue("@UpdatedAt" + w.Id, w.UpdatedAt != null ? w.UpdatedAt : string.Empty);
+                }
+
+                command.ExecuteNonQuery();
+            }
+        }        
+        con.Close();
     }
 }
